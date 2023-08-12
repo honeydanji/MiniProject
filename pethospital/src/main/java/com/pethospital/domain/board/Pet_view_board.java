b56e18d05fcdf2ed0f52ecfcef0c485899728ff2
honeydanji <tjdwls3680@naver.com>
2023-08-12 21:52:55 +0900

    # View (상위좋아요 및 최신게시글 ) 읽어오기

--- pethospital/src/main/java/com/pethospital/domain/board/Pet_view_board.java
+++ pethospital/src/main/java/com/pethospital/domain/board/Pet_view_board.java
@@ -3,11 +3,11 @@ package com.pethospital.domain.board;
 import java.util.Date;
 
 import javax.annotation.concurrent.Immutable;
-import javax.persistence.Table;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityResult;
 import jakarta.persistence.Id;
+import jakarta.persistence.SqlResultSetMapping;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,23 +16,20 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
-@Table(name = "combined_boards")
 @Immutable // 이 엔티티는 읽기 전용입니다 (뷰)
-//@Subselect("SELECT * FROM Combined_Boards") // 뷰 이름을 맞게 수정
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @Builder
-//@SqlResultSetMapping(name = "CombinedBoardMapping", entities = @EntityResult(entityClass = Pet_view_board.class))
+@SqlResultSetMapping(name = "CombinedBoardMapping", entities = @EntityResult(entityClass = Pet_view_board.class))
 public class Pet_view_board {
 
 	private String source;
 	
 	@Id
 	private int id; // ID 열로 사용될 변수
-	@Column(name = "user_id")
 	private String userId;
 	private String nickname;
 	private String title;
@@ -43,4 +40,7 @@ public class Pet_view_board {
 	private String imagefile;
 	private int views;
 	private int likes;
-}
+} 
+
+	//@SqlResultSetMapping(name = "CombinedBoardMapping", entities = @EntityResult(entityClass = Pet_view_board.class))
+	//@Subselect("SELECT * FROM Combined_Boards") // 뷰 이름을 맞게 수정
--- pethospital/src/main/java/com/pethospital/repository/board/Pet_view_board_Repository.java
+++ pethospital/src/main/java/com/pethospital/repository/board/Pet_view_board_Repository.java
@@ -9,17 +9,17 @@ import com.pethospital.domain.board.Pet_view_board;
 
 public interface Pet_view_board_Repository extends JpaRepository<Pet_view_board, Integer> {
 	
-	// 좋아요순 게시글 반환
-	@Query("SELECT * FROM Combined_Boards ORDER BY likes DESC LIMIT 6")
-    List<Pet_view_board> findTopLikedPosts();
+	// // 좋아요순 게시글 반환
+	// @Query("SELECT * FROM Combined_Boards ORDER BY likes DESC LIMIT 6")
+    // List<Pet_view_board> findTopLikedPosts();
     
-//    @Query(value = "SELECT * FROM Combined_Boards ORDER BY likes DESC LIMIT 6", nativeQuery = true)
-//    List<Pet_view_board> findTopLikedPosts();
+   @Query(value = "SELECT * FROM Combined_Boards ORDER BY likes DESC LIMIT 6", nativeQuery = true)
+   List<Pet_view_board> findTopLikedPosts();
     
-    // 최신 게시글 5개
-    @Query("SELECT * FROM Combined_Boards ORDER BY regdate DESC LIMIT 5")
-    List<Pet_view_board> findRecentBoards();
+    // // 최신 게시글 5개
+    // @Query("SELECT * FROM Combined_Boards ORDER BY regdate DESC LIMIT 5")
+    // List<Pet_view_board> findRecentBoards();
     
-//    @Query(value = "SELECT * FROM Combined_Boards ORDER BY regdate DESC LIMIT 5", nativeQuery = true)
-//    List<Pet_view_board> findRecentBoards();
+   @Query(value = "SELECT * FROM Combined_Boards ORDER BY regdate DESC LIMIT 5", nativeQuery = true)
+   List<Pet_view_board> findRecentBoards();
 }
