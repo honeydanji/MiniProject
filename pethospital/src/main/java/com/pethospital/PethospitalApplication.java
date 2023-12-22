822ab867e42111a5b80dc1f0d5e405dfe7fe03df
honeydanji <tjdwls3680@naver.com>
2023-12-22 17:14:46 +0900

    FEAT : Add Bean - BCryptPasswordEncoder
    // Description
    - 프로그램 실행시 BCryptPasswordEncoder 객체를 Bean 으로 등록하면 다른 객체에서 BCryptPasswordEncoder 의존성을 주입받을 수 있음.

--- pethospital/src/main/java/com/pethospital/PethospitalApplication.java
+++ pethospital/src/main/java/com/pethospital/PethospitalApplication.java
@@ -2,6 +2,8 @@ package com.pethospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class PethospitalApplication {
@@ -9,4 +11,10 @@ public class PethospitalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PethospitalApplication.class, args);
 	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
