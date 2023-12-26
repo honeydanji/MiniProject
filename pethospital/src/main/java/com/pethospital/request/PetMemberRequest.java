43d33c9777166005e47bf0a127b166ec651401e4
honeydanji <tjdwls3680@naver.com>
2023-12-26 22:42:04 +0900

    FEAT : Add member request object
    // Description
    - 요청 데이터를 받아서 유효성 검사를 진행하는 클래스
    - Dto 를 활용해도 되지만 기능을 분리하기 위해 따로 request 객체를 생성

---
+++ pethospital/src/main/java/com/pethospital/request/PetMemberRequest.java
@@ -0,0 +1,27 @@
+package com.pethospital.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import jakarta.validation.constraints.NotNull;
+@Data
+public class PetMemberRequest {
+    @NotNull(message = "Name Cannot be null")
+    @Size(min = 2, message = "Name not be less than two characters")
+    private String name;
+
+    @NotNull(message = "Nickname cannot be null")
+    @Size(min = 2, message = "Nickname not be than less two characters")
+    private String nickName;
+
+    @NotNull(message = "userId cannot be null")
+    private String userId;
+
+    @NotNull(message = "Password cannot be null")
+    @Size(min = 8, message = "Password must be equal or grater than 8 characters")
+    private String password;
+
+    @NotNull(message = "Email cannot be null")
+    @Email
+    private String email;
+}
