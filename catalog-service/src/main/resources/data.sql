6cca46322221bbe311c98660158e863601ee5801
honeydanji <tjdwls3680@naver.com>
2023-12-29 13:13:20 +0900

    FEAT(CatalogService) : Add Create SQL
    - 프로그램 실행 시 테스트용 데이터 자동 생성

---
+++ catalog-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into catalog(product_id, product_name, stock, unit_price) values ('CATALOG-001', 'Berlin', 100, 1500);
+insert into catalog(product_id, product_name, stock, unit_price) values ('CATALOG-002', 'Seoul', 1100, 100);
+insert into catalog(product_id, product_name, stock, unit_price) values ('CATALOG-003', 'Tokyo', 200, 15300);
