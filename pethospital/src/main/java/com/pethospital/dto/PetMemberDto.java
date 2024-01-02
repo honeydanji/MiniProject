0551cdbf0d7f3a4e9d345b76ba3d9e956f9c8c7e
honeydanji <tjdwls3680@naver.com>
2024-01-02 22:27:37 +0900

    STYLE : 필드 라인 변경

--- pethospital/src/main/java/com/pethospital/dto/PetMemberDto.java
+++ pethospital/src/main/java/com/pethospital/dto/PetMemberDto.java
@@ -1,14 +1,13 @@
 package com.pethospital.dto;
 
-
 import lombok.Data;
 
 @Data
 public class PetMemberDto {
 	private String name;
+	private String nickname;
 	private String userId;
 	private String password;
-	private String nickname;
 	private String email;
 	private String mobilePhone;
 	private String address;
