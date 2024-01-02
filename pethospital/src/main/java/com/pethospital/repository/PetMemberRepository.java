83f49f5002d4b9ff87f076286ea33769894ad89d
honeydanji <tjdwls3680@naver.com>
2024-01-02 22:33:56 +0900

    STYLE : 주석 제거 및 라인 정리

--- pethospital/src/main/java/com/pethospital/repository/PetMemberRepository.java
+++ pethospital/src/main/java/com/pethospital/repository/PetMemberRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.pethospital.domain.PetMember;
 
 public interface PetMemberRepository extends JpaRepository<PetMember, Integer> {
-    // 프론트에서 전송한 회원가입 정보를 Pet_member 저장.
-	
 	PetMember findByUserId(String userId);
 }
