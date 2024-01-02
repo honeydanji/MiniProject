97793dd937a486e499ca473683f2ca974d94478f
honeydanji <tjdwls3680@naver.com>
2024-01-02 23:07:45 +0900

    FEAT : PetMemberDto를 받아 PetMember(Entity)로 매핑후 Repository 전달

--- pethospital/src/main/java/com/pethospital/service/PetMemberService.java
+++ pethospital/src/main/java/com/pethospital/service/PetMemberService.java
@@ -1,6 +1,8 @@
 package com.pethospital.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.pethospital.domain.PetMember;
@@ -8,32 +10,18 @@ import com.pethospital.dto.PetMemberDto;
 import com.pethospital.repository.PetMemberRepository;
 
 @Service
+@RequiredArgsConstructor
 public class PetMemberService {
+
 	private final PetMemberRepository petMemberRepository;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	@Autowired
-	public PetMemberService(PetMemberRepository petMemberRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
-		this.petMemberRepository = petMemberRepository;
-		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-	}
-
-    // 회원 정보 저장
     public void registerPetMember(PetMemberDto petMemberDto) {
-    	PetMember petMember = new PetMember();
-    	
-    	// 필수사항
-    	petMember.setName(petMemberDto.getName());
-    	petMember.setUserId(petMemberDto.getUserId());
-    	petMember.setPassword(bCryptPasswordEncoder.encode(petMemberDto.getPassword())); // 암호화하기
-    	petMember.setNickname(petMemberDto.getNickname());
-    	petMember.setEmail(petMemberDto.getEmail());
-    	
-     	// 선택사항
-    	petMember.setMobilePhone(petMemberDto.getMobilePhone());
-    	petMember.setAddress(petMemberDto.getAddress()); 
-    	
-    	// 권한부여
+		ModelMapper mapper = new ModelMapper();
+		mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+    	PetMember petMember = mapper.map(petMemberDto, PetMember.class);
+		petMember.setPassword(bCryptPasswordEncoder.encode(petMemberDto.getPassword()));
     	petMember.setRole("ROLE_MEMBER");
 
 		petMemberRepository.save(petMember);
