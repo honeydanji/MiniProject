e2d145fa9420b4e0ea3f197e231dd78b60c5118e
honeydanji <tjdwls3680@naver.com>
2024-01-05 18:23:05 +0900

    TEST : RequestLogin 필드 유효성 검사 통과

---
+++ pethospital/src/test/java/com/pethospital/vo/RequestLoginTest.java
@@ -0,0 +1,61 @@
+package com.pethospital.vo;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestLoginTest {
+
+    private static ValidatorFactory factory;
+    private static Validator validator;
+
+    @BeforeAll
+    public static void init() {
+        factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @AfterAll
+    public static void close() {
+        factory.close();
+    }
+
+    @Test
+    @DisplayName("로그인 요청 데이터 아이디가 비어있는 경우")
+    void userIdValidTest() {
+        RequestLogin login = new RequestLogin();
+        login.setPassword("testtest");
+
+        Set<ConstraintViolation<RequestLogin>> violations = validator.validate(login);
+
+        assertThat(violations).isNotEmpty();
+        violations
+                .forEach(error -> {
+                    assertThat(error.getMessage()).isEqualTo("userId cannot be empty");
+                });
+    }
+
+    @Test
+    @DisplayName("로그인 요청 데이터 비밀번호가 비어있는 경우")
+    void passwordValidTest() {
+        RequestLogin login = new RequestLogin();
+        login.setUserId("test");
+
+        Set<ConstraintViolation<RequestLogin>> violations = validator.validate(login);
+
+        assertThat(violations).isNotEmpty();
+        violations
+                .forEach(error -> {
+                    assertThat(error.getMessage()).isEqualTo("Password cannot be empty");
+                });
+    }
+}
