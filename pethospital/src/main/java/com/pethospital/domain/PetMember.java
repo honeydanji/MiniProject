12eefcc4fa3f8697f1567f63f10a94eb9cb20ee4
honeydanji <tjdwls3680@naver.com>
2024-01-05 16:34:09 +0900

    REFACTOR : 회원가입 기본정보 "name" 필드 기본 설정 변경

--- pethospital/src/main/java/com/pethospital/domain/PetMember.java
+++ pethospital/src/main/java/com/pethospital/domain/PetMember.java
@@ -19,7 +19,7 @@ public class PetMember {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-    @Column(nullable = false, unique = true)
+    @Column(nullable = false)
     private String name;
     @Column(nullable = false, unique = true)
     private String nickname;
