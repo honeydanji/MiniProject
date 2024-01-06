fd8cf011a613677b563bff16c5ae60bca8dfe94b
honeydanji <tjdwls3680@naver.com>
2024-01-06 15:26:32 +0900

    DELETE

--- catalog-service/src/main/java/com/catalogservice/jpa/CatalogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.catalogservice.jpa;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
-    CatalogEntity findByProductId(String productId);
-}
